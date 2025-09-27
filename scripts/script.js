0bbcaa223d5e15d8f1d295f8fd46fd21fe2c383e
valliert <valliert@rose-hulman.edu>
2025-09-26 22:22:13 -0400

    footer

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,7 @@
-function makeHeader() {
-    let header = document.createElement("header");
+let header = document.createElement("header");
+let footer = document.createElement("footer");
 
+function makeHeader() {   
     let name = document.createTextNode("Riley Vallier");
     name.className = "tab";
     header.append(name);
@@ -27,7 +28,25 @@ function makeHeader() {
 
 }
 
+function makeFooter() {
+    footer.append(document.createElement("hr"));
+    footer.append(document.createElement("hr"));
+
+    let socials = {"LinkedIn": "https://www.linkedin.com/in/riley-vallier-1a0649293", "Handshake": "https://rose-hulman.joinhandshake.com/profiles/fb7bgd"};
+    socials.forEach((page, site) => {
+        let link = document.createElement("a");
+        link.textContent = page;
+        let file = site;
+        link.href = file;
+        link.className = "tab";
+        footer.append(link);
+    });
+
+    document.body.append(footer);
+}
+
 window.addEventListener("load", (event) => {
     console.log(event)
     makeHeader();
+    makeFooter();
 })
