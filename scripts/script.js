1b82c440ad6ad07aa7d9514ffd87ad91bd6254f1
valliert <valliert@rose-hulman.edu>
2025-09-26 22:28:31 -0400

    footer

--- scripts/script.js
+++ scripts/script.js
@@ -33,7 +33,7 @@ function makeFooter() {
     footer.append(document.createElement("hr"));
 
     let socials = {"LinkedIn": "https://www.linkedin.com/in/riley-vallier-1a0649293", "Handshake": "https://rose-hulman.joinhandshake.com/profiles/fb7bgd"};
-    for (const [page, site] of socials.entries()) {
+    for (const [page, site] in socials) {
         let link = document.createElement("a");
         link.textContent = page;
         let file = site;
